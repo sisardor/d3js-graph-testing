052592141978561905c1c3fb9192ae0ff1162fe2
Sardor Isakov <sisardor@gmail.com>
2014-12-19 18:12:08 -0800

    update

--- app/js/controllers/example.js
+++ app/js/controllers/example.js
@@ -7,7 +7,7 @@ var controllersModule = require('./_index');
  */
 function ExampleCtrl() {
 
-console.log("ExampleCtrl");
+console.log('ExampleCtrl');
 
 
 
@@ -22,14 +22,14 @@ var data = [
   {snapshot_date : '2013-07-01', rwa_total: 123234.454, notion_total: 5545.11, loss_total: 143},
 ]
 
-// d3.select("#chart").selectAll("div")
+// d3.select('#chart').selectAll('div')
 //     .data(dataset)
 //     .enter()
-//     .append("div")
-//     .attr("class", "bar")
-// .style("height", function(d) {
+//     .append('div')
+//     .attr('class', 'bar')
+// .style('height', function(d) {
 //     var barHeight = d * 5;  //Scale up by factor of 5
-//     return barHeight + "px";
+//     return barHeight + 'px';
 // });
 
 
@@ -52,22 +52,22 @@ var y = d3.scale.linear()
     .rangeRound([height, 0]);
 
 var color = d3.scale.ordinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+    .range(['#98abc5', '#8a89a6', '#7b6888', '#6b486b', '#a05d56', '#d0743c', '#ff8c00']);
 
 var xAxis = d3.svg.axis()
     .scale(x)
-    .orient("bottom");
+    .orient('bottom');
 
 var yAxis = d3.svg.axis()
     .scale(y)
-    .orient("left")
-    .tickFormat(d3.format(".2s"));
+    .orient('left')
+    .tickFormat(d3.format('.2s'));
 
-var svg = d3.select("#chart2").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select('#chart2').append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
 
 
@@ -75,7 +75,7 @@ var svg = d3.select("#chart2").append("svg")
   color.domain(
       d3.keys( data[0] )
         .filter(function(key) { 
-          return key !== "State"; 
+          return key !== 'State'; 
         })
     );
 
@@ -96,57 +96,57 @@ var svg = d3.select("#chart2").append("svg")
   x.domain(data.map(function(d) { return d.State; }));
   y.domain([0, d3.max(data, function(d) { return d.total; })]);
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
+  svg.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + height + ')')
       .call(xAxis);
 
-  svg.append("g")
-      .attr("class", "y axis")
+  svg.append('g')
+      .attr('class', 'y axis')
       .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Population");
+    .append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 6)
+      .attr('dy', '.71em')
+      .style('text-anchor', 'end')
+      .text('Population');
 
-  var state = svg.selectAll(".state")
+  var state = svg.selectAll('.state')
       .data(data)
-    .enter().append("g")
-      .attr("class", "g")
-      .attr("transform", function(d) { return "translate(" + x(d.State) + ",0)"; });
+    .enter().append('g')
+      .attr('class', 'g')
+      .attr('transform', function(d) { return 'translate(' + x(d.State) + ',0)'; });
 
-  state.selectAll("rect")
+  state.selectAll('rect')
       .data(function(d) { return d.rwa_total; })
-    .enter().append("rect")
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.y1); })
-      .attr("height", function(d) { return y(d.y0) - y(d.y1); })
-      .style("fill", function(d) { return color(d.name); });
+    .enter().append('rect')
+      .attr('width', x.rangeBand())
+      .attr('y', function(d) { return y(d.y1); })
+      .attr('height', function(d) { return y(d.y0) - y(d.y1); })
+      .style('fill', function(d) { return color(d.name); });
 
-  var legend = svg.selectAll(".legend")
+  var legend = svg.selectAll('.legend')
       .data(color.domain().slice().reverse())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+    .enter().append('g')
+      .attr('class', 'legend')
+      .attr('transform', function(d, i) { return 'translate(0,' + i * 20 + ')'; });
 
-  legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
+  legend.append('rect')
+      .attr('x', width - 18)
+      .attr('width', 18)
+      .attr('height', 18)
+      .style('fill', color);
 
-  legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
+  legend.append('text')
+      .attr('x', width - 24)
+      .attr('y', 9)
+      .attr('dy', '.35em')
+      .style('text-anchor', 'end')
       .text(function(d) { return d; });
 
 // d3.data(json,function(data) { 
 
-//   color.domain(d3.keys(data[0]).filter(function(key) { return key !== "State"; }));
+//   color.domain(d3.keys(data[0]).filter(function(key) { return key !== 'State'; }));
 
 
 
