d9f99ad22f8a0827331c8a9ae7a9dde4d298fee4
Sardor Isakov <sisardor@gmail.com>
2014-12-22 18:19:23 -0800

    Hot update

--- app/js/controllers/example.js
+++ app/js/controllers/example.js
@@ -30,11 +30,12 @@ response.reduce(function(previousValue, currentValue, index, array) {
   return;
 });
 
-
-var graph = new EraGraph(json_data, 'stacked', '#chart');
+var graph = new EraGraph(json_data, 'grouped', '#chart1');
 graph.drawGraph();
 
 
+var graph = new EraGraph(json_data, 'stacked', '#chart2');
+graph.drawGraph();
 
 
 
@@ -43,16 +44,16 @@ graph.drawGraph();
 controllersModule.controller('ExampleCtrl', ExampleCtrl);
 
 function EraGraph(json_data, type, selector) {
-	this.json_data = json_data;
+	this.json_data = JSON.parse(JSON.stringify(json_data));
 	this.type = type;
 	this.selector = selector;
 
 
 	var margin = {top: 20, right: 20, bottom: 30, left: 40},
-	    width  = 960 - margin.left - margin.right,
-	    height = 500 - margin.top - margin.bottom;
+	    width  = 460 - margin.left - margin.right,
+	    height = 400 - margin.top - margin.bottom;
 
-	var x0 = d3.scale.ordinal()
+	var x = d3.scale.ordinal()
 	    .rangeRoundBands([0, width], .1);
 
 	var x1 = d3.scale.ordinal();
@@ -64,7 +65,7 @@ function EraGraph(json_data, type, selector) {
 	    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
 	var xAxis = d3.svg.axis()
-	    .scale(x0)
+	    .scale(x)
 	    .orient("bottom");
 
 	var yAxis = d3.svg.axis()
@@ -86,53 +87,167 @@ function EraGraph(json_data, type, selector) {
 		.html("a simple tooltip <br>hello");
 
 	var moneyFormat = d3.format(".2s");
-	var rowNames = d3.keys(this.json_data[0]).filter(function(key) { return key !== "snapshot_date"; }); 
+	
 
 
 	this.drawGraph = function() {
+		if (this.type === 'stacked') {
+			color.domain(d3.keys(this.json_data[0]).filter(function(key) { return key !== "snapshot_date"; }).slice().reverse() )
 
-		this.json_data.forEach(function(d) {
-		  	d.amount = rowNames.map(function(name){
-		  		return {name: name, value: +d[name]};
-		  	});
+			  	this.json_data.forEach(function(d){
+				  	var y0 = 0;
+				  	d.amount = color.domain().map(function(name) {
+				  		return {name: name, y0:y0, y1:y0 += +d[name], value: d[name] }
+				  	});
+				  	d.total = d.amount[d.amount.length - 1].y1;
+			  	})
 
-		})
+				this.json_data.sort(function(a, b) { return new Date(a.snapshot_date) - new Date(b.snapshot_date) ; });
+			  	x.domain(this.json_data.map(function(d) {return d.snapshot_date; }));
 
-	 	x0.domain(this.json_data.map(function(d) { return d.snapshot_date; }));
-		x1.domain(rowNames).rangeRoundBands([0, x0.rangeBand()]);
-		y.domain([0, d3.max(this.json_data, function(d) { return d3.max(d.amount, function(d) { return d.value; }); })]);
+			  	y.domain([0, d3.max(this.json_data, function(d) { return d.total; })]);
+
+			  svg.append("g")
+			      .attr("class", "x axis")
+			      .attr("transform", "translate(0," + height + ")")
+			      .call(xAxis);
+
+			  svg.append("g")
+			      .attr("class", "y axis")
+			      .call(yAxis)
+			    .append("text")
+			      .attr("transform", "rotate(-90)")
+			      .attr("y", 6)
+			      .attr("dy", ".71em")
+			      .style("text-anchor", "end")
+			      .text("Amount in USD");
+
+			  var state = svg.selectAll(".state")
+			      .data(this.json_data)
+			    .enter().append("g")
+			      .attr("class", "g")
+			      .attr("transform", function(d) { return "translate(" + x(d.snapshot_date) + ",0)"; })
+			      .on("mouseover", function(d){
+			        var html = "";
+			     	var i =  d.amount.length;
+			        while(i--) {
+			          html+= "<span style=\"color:" + color(d.amount[i].name)+"\">" + d.amount[i].name + ": " + moneyFormat(d.amount[i].value) + " </span>"
+			        };
+			        tooltip.html("<div class=\"graph-tooltip\">Assessment Date: " + d.snapshot_date + "<br/>" + html + "</div>")
+			        return tooltip.style("visibility", "visible");})
+			      .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+			      .on("mouseout", function(){return tooltip.style("visibility", "hidden");})
+
+			  state.selectAll("rect")
+			      .data(function(d) { return d.amount; })
+			    .enter().append("rect")
+			      .attr("width", x.rangeBand())
+			      
+			      .attr("y", function(d) { return y(d.y1); })
+			      .attr("height", function(d) { return y(d.y0) - y(d.y1); })
+			      .style("fill", function(d) { return color(d.name); });
+
+			  var legend = svg.selectAll(".legend")
+			      .data(color.domain())
+			    .enter().append("g")
+			      .attr("class", "legend")
+			      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+			  legend.append("rect")
+			      .attr("x", width - 18)
+			      .attr("width", 18)
+			      .attr("height", 18)
+			      .style("fill", color);
+
+			  legend.append("text")
+			      .attr("x", width - 24)
+			      .attr("y", 9)
+			      .attr("dy", ".35em")
+			      .style("text-anchor", "end")
+			      .text(function(d) { return d; });
 
 
 
-		var state = svg.selectAll(".state")
-			.data(this.json_data)
+		} else if (this.type === 'grouped') {
+			var rowNames = d3.keys(this.json_data[0]).filter(function(key) { return key !== "snapshot_date"; }); 
+
+			  	this.json_data.forEach(function(d) {
+
+					d.amount = rowNames.map(function(name){
+						return {name: name, value: +d[name]};
+					})
+
+			  	})
+
+				this.json_data.sort(function(a, b) { return new Date(a.snapshot_date) - new Date(b.snapshot_date) ; });
+				x.domain(this.json_data.map(function(d) {return d.snapshot_date; }));
+				x1.domain(rowNames).rangeRoundBands([0, x.rangeBand()]);
+				y.domain([0, d3.max(this.json_data, function(d) { return d3.max(d.amount, function(d) { return d.value; }); })]);
+
+			  svg.append("g")
+				  .attr("class", "x axis")
+				  .attr("transform", "translate(0," + height + ")")
+				  .call(xAxis);
+
+			  svg.append("g")
+				  .attr("class", "y axis")
+				  .call(yAxis)
+				.append("text")
+				  .attr("transform", "rotate(-90)")
+				  .attr("y", 6)
+				  .attr("dy", ".71em")
+				  .style("text-anchor", "end")
+				  .text("Amount in USD");
+
+			  var state = svg.selectAll(".state")
+				  .data(this.json_data)
 				.enter().append("g")
-				.attr("class", "g")
-				.attr("transform", function(d) { return "translate(" + x0(d.snapshot_date) + ",0)"; })
-			.on("mouseover", function(d){
-				var html = "";
-				var i =  d.amount.length;
-				while(i--) {
-				  html+= "<span style=\"color:" + color(d.amount[i].name)+"\">" + d.amount[i].name + ": " + moneyFormat(d.amount[i].value) + " </span>"
-				};
-				tooltip.html("<div class=\"graph-tooltip\">Assessment Date: " + d.snapshot_date + "<br/>" + html + "</div>")
-				return tooltip.style("visibility", "visible");
-			})
-			.on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
-			.on("mouseout", function(){return tooltip.style("visibility", "hidden");})
+				  .attr("class", "g")
+				  .attr("transform", function(d) { return "translate(" + x(d.snapshot_date) + ",0)"; })
+				  .on("mouseover", function(d){
+					var html = "";
+				  	var i =  d.amount.length;
+					while(i--) {
+					  html+= "<span style=\"color:" + color(d.amount[i].name)+"\">" + d.amount[i].name + ": " + moneyFormat(d.amount[i].value) + " </span>"
+					};
+					tooltip.html("<div class=\"graph-tooltip\">Assessment Date: " + d.snapshot_date + "<br/>" + html + "</div>")
+					return tooltip.style("visibility", "visible");})
+				  .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+				  .on("mouseout", function(){return tooltip.style("visibility", "hidden");})
 
-		state.selectAll("rect")
-		  	.data(function(d) { return d.amount; })
+			  state.selectAll("rect")
+				  .data(function(d) { return d.amount; })
+				.enter().append("rect")
+				  .attr("width", x1.rangeBand())
+				  .attr("x", function(d) { return x1(d.name); })
+				  .attr("y", function(d) { return y(d.value); })
+				  .attr("height", function(d) { return height - y(d.value); })
+				  .style("fill", function(d) { return color(d.name); });
 
-		.enter().append("rect")
-			.attr("width", x1.rangeBand())
-			.attr("x", function(d) { return x1(d.name); })
-			.attr("y", function(d) { return y(d.value); })
-			.attr("height", function(d) { return height - y(d.value); })
-			.style("fill", function(d) { return color(d.name); });
+			  var legend = svg.selectAll(".legend")
+				  .data(rowNames.slice().reverse())
+				.enter().append("g")
+				  .attr("class", "legend")
+				  .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-		drawAxis();
-		drawLegend(rowNames.slice().reverse())
+
+			  legend.append("rect")
+				  .attr("x", width - 18)
+				  .attr("width", 18)
+				  .attr("height", 18)
+				  .style("fill", color);
+
+			  legend.append("text")
+				  .attr("x", width - 24)
+				  .attr("y", 9)
+				  .attr("dy", ".35em")
+				  .style("text-anchor", "end")
+				  .text(function(d) { console.log(d);return d; });
+		}
+
+		
+
+		
 
 	}
 
@@ -151,27 +266,6 @@ function EraGraph(json_data, type, selector) {
 		  .attr("dy", ".71em")
 		  .style("text-anchor", "end")
 		  .text("Amount in USD");
-	}
-
-	function drawLegend(data) {
-		var legend = svg.selectAll(".legend")
-			.data(data)
-		  .enter().append("g")
-			.attr("class", "legend")
-			.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-		legend.append("rect")
-		  .attr("x", width - 18)
-		  .attr("width", 18)
-		  .attr("height", 18)
-		  .style("fill", color);
-
-		legend.append("text")
-		  .attr("x", width - 24)
-		  .attr("y", 9)
-		  .attr("dy", ".35em")
-		  .style("text-anchor", "end")
-		  .text(function(d) { return d; });
 	}
 
 }
