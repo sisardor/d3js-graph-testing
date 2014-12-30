a8b3c18d80c44d5f9a8d2163a46cc092012aaca7
Sardor Isakov <sisardor@gmail.com>
2014-12-29 17:24:46 -0800

    graph update

--- app/js/controllers/example.js
+++ app/js/controllers/example.js
@@ -35,6 +35,9 @@ var controllersModule = require('./_index');
 	// console.log("--------");
 
 	//D3graph.generate(json_data, 'stacked', '#chart1')
+
+
+	var g = new EraGraph(response,'#chart1');
 }
 
 controllersModule.controller('ExampleCtrl', ExampleCtrl);
@@ -65,37 +68,25 @@ controllersModule.controller('ExampleCtrl', ExampleCtrl);
 
 
 
-
-(function() {
+(function(d3) {
 	var EraGraph;
 
 	EraGraph = (function() {	
-		var response = [
-			["snapshot_date", "Notion Total", "RWA Total", "Expected Loss Total"],
-			["2014-01-01", 98, 8, 4.8],
-			["2013-12-01", 27, 8, 4.0],
-			["2013-11-01", 47, 7, 4.8],
-			["2013-10-01", 56, 7, 4.0],
-			["2013-09-01", 46, 8, 4.8],
-			["2013-08-01", 45, 6, 4.0],
-			["2013-07-01", 1, 7, 4.8],
-			["2013-06-01", 1, 7, 74.0],
-		];
-
-		function EraGraph(selector){
+		function EraGraph(response, selector){
+			addSelect(selector);
 			this.selector = selector;
 			this._array = this.parse_data(response);
-			this.tooltip_legend = this.parse_data(response);
-			var n = 3; // number of layers
-			var m = 8; // number of samples per layer
+			this.tooltip_legend = this.get_legend_labels(response);
+			
 			var stack = d3.layout.stack();
 			this.layers = stack( this._array );
+
 			this.yGroupMax = d3.max(this.layers, function(layer) { return d3.max(layer, function(d) { return d.y; }); });
 			this.yStackMax = d3.max(this.layers, function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); });
-			this.options = 0;
 
-			var widthX = document.getElementById(this.selector).offsetWidth;
-			var heightX = document.getElementById(this.selector).offsetHeight;
+
+			var widthX = document.getElementById(this.selector.replace('#','')).offsetWidth;
+			var heightX = document.getElementById(this.selector.replace('#','')).offsetHeight;
 
 			this.margin = {top: 40, right: 20, bottom: 40, left: 35};
 			this.width = widthX - this.margin.left - this.margin.right;
@@ -111,14 +102,14 @@ controllersModule.controller('ExampleCtrl', ExampleCtrl);
 			    .range([this.height, 0]);
 
 			this.color = d3.scale.ordinal().range(["#a05d56","#8a89a6","#98abc5" ]);
-			
+			//this.color = d3.scale.linear().domain([0, n - 1]).range(["#556","#aad" ]);
 			var ageNames = d3.keys(this.tooltip_legend).filter(function(key) { return key !== "State"; });
 			this.color.domain(d3.keys(this.tooltip_legend[ageNames[0]]).filter(function(key) { return key !== "State"; }).slice().reverse() )
-			console.log(ageNames)
+			
 
-			this.tooltip = d3.select( "#chart1" )
+			this.tooltip = d3.select( this.selector )
 			 			.append("div")
-			 			.style("position", "absolute")
+			 			.style("position", "relative")
 			 			.style("z-index", "10")
 			 			.style("visibility", "hidden")
 			 			.html("a simple tooltip <br>hello");
@@ -135,16 +126,91 @@ controllersModule.controller('ExampleCtrl', ExampleCtrl);
 
 
 			this.generate_axis();
+			this.generate_bars();
+			this.generate_legends();
 		}
 
-		EraGraph.prototype.generate = function(){
-			console.log(this.margin)
-			this.generate_axis();
+
+		EraGraph.prototype.generate_bars = function(){
+			var x = this.x;
+			var y = this.y;
+			var n = this.layers.length;
+			var yStackMax = this.yStackMax;
+			var yGroupMax = this.yGroupMax;
+			var height = this.height;
+			var divID = this.selector;
+			var color = this.color;
+			var tooltip_legend = this.tooltip_legend;
+			var tooltip = this.tooltip;
+			var layer = this.svg.selectAll(".layer")
+			    .data(this.layers)
+			  .enter().append("g")
+			    .attr("class", "layer")
+			    .style("fill", function(d, i) { return color(d[i].category); });
+			var rect = layer.selectAll("rect")
+			    .data(function(d) { return d; })
+			  .enter().append("rect")
+			    .attr("x", function(d) {  return x(d.total); })
+			    .attr("y", this.height)
+			    .attr("width", x.rangeBand())
+			    .attr("height", 0)
+			.on("mouseover", function(d){
+				var html = '';
+				for(var key in tooltip_legend[d.total]) {
+					html+= '<span style="color:' + color(key) +'">' + key + ': ' + tooltip_legend[d.total][key] + ' </span>';
+				}
+				tooltip.html('<div class="graph-tooltip">Snapshot Date: ' + d.total + '<br/>' + html + '</div>')
+				return tooltip.style('visibility', 'visible');
+			})
+			.on("mousemove", function(){console.log(event.pageY-10, event.pageX+10 ); return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+			.on("mouseout", function(){return tooltip.style("visibility", "hidden");})
+
+			rect.transition()
+			    .delay(function(d, i) { return i * 10; })
+			    .attr("y", function(d) { return y(d.y0 + d.y); })
+			    .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); });
+
+			d3.selectAll(divID + " input").on("change", change);
+
+			var timeout = setTimeout(function() {
+				d3.select(divID + " input[value=\"grouped\"]").property("checked", true).each(change);
+			}, 2000);
+			
+
+			function change() {
+			  clearTimeout(timeout);
+			  if (this.value === "grouped") transitionGrouped();
+			  else transitionStacked();
+			}
+
+			function transitionGrouped() {
+			  y.domain([0, yGroupMax]);
+
+			  rect.transition()
+			      .duration(500)
+			      .delay(function(d, i) { return i * 10; })
+			      .attr("x", function(d, i, j) { return x(d.total) + x.rangeBand() / n * j; })
+			      .attr("width", x.rangeBand() / n)
+			    .transition()
+			      .attr("y", function(d) { return y(d.y); })
+			      .attr("height", function(d) { return height - y(d.y); });
+			}
+
+			function transitionStacked() {
+			  y.domain([0, yStackMax]);
+
+			  rect.transition()
+			      .duration(500)
+			      .delay(function(d, i) { return i * 10; })
+			      .attr("y", function(d) { return y(d.y0 + d.y); })
+			      .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); })
+			    .transition()
+			      .attr("x", function(d) { return x(d.total); })
+			      .attr("width", x.rangeBand());
+			}
 		};
 
 		EraGraph.prototype.generate_axis = function() {
-			console.log('generate_axis');
-
 			var xAxis = d3.svg.axis().scale(this.x).tickSize(0).tickPadding(6).orient("bottom");
 			var yAxis = d3.svg.axis().scale(this.y).orient("left").tickFormat(d3.format(".2s"));
 			this.svg.append("g")
@@ -190,42 +256,32 @@ controllersModule.controller('ExampleCtrl', ExampleCtrl);
 			.attr("dy", ".35em")
 			.style("text-anchor", "end")
 			.text(function(d) { return d; });
-		}
+		};
 
 		EraGraph.prototype.parse_data = function(data){
 			var _array = [[],[],[]];
-			var tooltip_legend = {};
-			///////---------------///////////
-			response.reduce(function(previousValue, currentValue, index, array) {
-						var t = {};
-						t.total = currentValue[0];
-						t.y = currentValue[1];
-						t.category = array[0][1];
-						_array[2].push(t);
+			data.reduce(function(previousValue, currentValue, index, array) {
+				_array[2].push({
+					total 	 : 	currentValue[0],
+					y 		 :  currentValue[1],
+					category : 	array[0][1]
+				} );
+				_array[1].push( {
+					total 	 : 	currentValue[0],
+					y 		 :  currentValue[2],
+					category : 	array[0][2]
+				} );
 
-						var t = {};
-						t.total = currentValue[0];
-						t.y = currentValue[2]
-						t.category = array[0][2];
-						_array[1].push(t);
-
-						var t = {};
-						t.total = currentValue[0];
-						t.y = currentValue[3]
-						t.category = array[0][3];
-						_array[0].push(t);
-
-						var _x = {};
-						_x[array[0][1]] = currentValue[1]
-						_x[array[0][2]] = currentValue[2]
-						_x[array[0][3]] = currentValue[3]
-						tooltip_legend[currentValue[0]] = _x
-
+				_array[0].push({
+					total 	 : 	currentValue[0],
+					y 		 :  currentValue[3],
+					category : 	array[0][3]
+				});
 			});
+			return _array;
 		};
 
 		EraGraph.prototype.get_legend_labels = function(data){
-			console.log(data)
 			var tooltip_legend = {};
 			data.reduce(function(previousValue, currentValue, index, array) {
 				var _x = {};
@@ -238,8 +294,21 @@ controllersModule.controller('ExampleCtrl', ExampleCtrl);
 			return tooltip_legend;
 		};
 
+		function addSelect(divId){
+			var innerHTML = '<form>'+
+							  '<label><input type="radio" name="mode" value="grouped"> Grouped</label>'+
+							  '<label><input type="radio" name="mode" value="stacked" checked> Stacked</label>'+
+							'</form>';
+			d3.select( divId )
+	 			.append("div")
+	 			.style("position", "absolute")
+	 			.style("z-index", "10")
+	 			.html(innerHTML);
+		}
+
+
 	 	return EraGraph;
   	})();
 
  	window.EraGraph = EraGraph
-}).call(this);
+})(d3);
